69c070f736a96604b10ffc519c901d2d61ee5af0
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-09-08 21:10:10 -0700

    added inital react components

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const source = require('vinyl-source-stream');
+const notify = require('gulp-notify');
+
+function handleErrors(){
+  notify.onError({
+    title: "compile error",
+    message: "<% error.message %>"
+  }).apply(this, arguments);
+  this.emit('end');
+} 
+
+gulp.task('build-client', () =>{
+  const bundler = watchify(browserify({
+    entries: ["./client/app.jsx"],
+    debug: true,
+    transform: [['babelify', {presets: ['es2015','react']}]]
+  }))
+
+  function rebundle(){
+    var stream = bundler.bundle();
+    return stream
+      .on('error', handleErrors)
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./client/build/'));
+  }
+
+  bundler.on('update', () => {
+    rebundle();
+    console.log('updated');
+  })
+
+  return rebundle();
+
+})
+
+gulp.task('default', ['build-client']);
+
