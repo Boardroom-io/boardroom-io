9683a85194bb815a1011e0bd4839f4fc63be82b1
Daniel King <daniel.oliver.king@gmail.com>
2016-09-09 13:05:46 -0700

    Two users can now draw semi-smoothly simultaneously

--- canvas/main.js
+++ canvas/main.js
@@ -1,5 +1,7 @@
 $(document).ready(() => {
 
+  const socket = io();
+
   function addEventListenersToCanvas() {
     const canvas = document.getElementById('drawMe');
     const rect = canvas.getBoundingClientRect();
@@ -7,39 +9,41 @@ $(document).ready(() => {
     context.strokeStyle = 'black';
     context.lineWidth = '5';
     context.lineJoin = 'round';
+    context.lineCap = 'round';
     let x;
     let y;
+    let prevX;
+    let prevY;
     let mouseDown = false;
     let locations = [];
 
     function startDraw(e) {
-      e.preventDefault();
       mouseDown = true;
+      prevX = e.pageX - rect.left;
+      prevY = e.pageY - rect.top;
     }
 
     function newDraw(e) {
-      e.preventDefault();
       if (mouseDown) {
         x = e.pageX - rect.left;
         y = e.pageY - rect.top;
 
-        locations.push({ x, y });
+        drawOnCanvas(prevX, prevY, x, y);
+        socket.emit('draw', {prevX, prevY, x, y});
+        prevX = x;
+        prevY = y;
 
-        drawOnCanvas(locations);
       }
     }
 
-    function drawOnCanvas(locationsArr) {
+    function drawOnCanvas(prevX, prevY, x, y) {
       context.beginPath();
-      context.moveTo(locationsArr[0].x, locationsArr[0].y);
-      for (let i = 1; i < locationsArr.length; i++) {
-        context.lineTo(locationsArr[i].x, locationsArr[i].y);
-      }
+      context.moveTo(prevX, prevY);
+      context.lineTo(x, y);
       context.stroke();
     }
 
     function endDraw(e) {
-      e.preventDefault();
       mouseDown = false;
       x = null;
       y = null;
@@ -50,10 +54,16 @@ $(document).ready(() => {
     canvas.addEventListener('mousemove', newDraw, false);
     canvas.addEventListener('mouseup', endDraw, false);
     canvas.addEventListener('mouseleave', endDraw, false);
-    canvas.addEventListener('touchstart', startDraw, false);
-    canvas.addEventListener('touchmove', newDraw, false);
-    canvas.addEventListener('touchend', endDraw, false);
-    canvas.addEventListener('touchleave', endDraw, false);
+
+    // FIXME: Figure out how to make canvas work with touchscreens
+    // canvas.addEventListener('touchstart', startDraw, true);
+    // canvas.addEventListener('touchmove', newDraw, true);
+    // canvas.addEventListener('touchend', endDraw, true);
+    // canvas.addEventListener('touchleave', endDraw, true);
+
+    socket.on('other client draw', coords => {
+      drawOnCanvas(coords.prevX, coords.prevY, coords.x, coords.y);
+    });
   }
 
   addEventListenersToCanvas();
---
+++ canvas/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+  socket.on('draw', coords => {
+    socket.broadcast.emit('other client draw', coords);
+  });
+});
+
+http.listen(3000, () => console.log('listening on *:3000'));
