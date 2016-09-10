bf0b94b683c872f739c253185f08bbae4be45a27
Daniel King <daniel.oliver.king@gmail.com>
2016-09-09 19:28:36 -0700

    Added comments

--- client/app.jsx
+++ client/app.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Top level React component for the text-chat window
 class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            helloWorld: 'Hello'
-        }
+  constructor(){
+    super();
+    this.state = {
+      helloWorld: 'Hello'
     }
+  }
 
-    render(){
-        return (
-            <div> Hello </div>
-        )
-    }
+  render(){
+    return (
+      <div> Hello </div>
+    )
+  }
 }
 
 
 ReactDOM.render(<App />, document.getElementById('chat-app'));
-
--- client/socket.js
+++ client/socket.js
@@ -1,5 +1,7 @@
-const io = require('socket.io-client');
+import io from 'socket.io-client';
 
+// Establish the socket connection to the server that served the page, and
+// export it to make it available to other JS files on the client side.
 const socket = io();
 
 module.exports = socket;
