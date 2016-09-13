a2fd7f73c69f2d1e549a7ca325aa263745b7369c
Jwookie55 <jwookie55@live.com>
2016-09-13 13:39:28 -0700

    linted all files

--- client/app.jsx
+++ client/app.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MessageForm from './components/message_form.jsx'
-import Message from './components/message.jsx'
-import MessageContainer from './components/message_container.jsx'
-import {socket, p2p} from './socket.js'
+import MessageForm from './components/message_form.jsx';
+import Message from './components/message.jsx';
+import MessageContainer from './components/message_container.jsx';
+import { socket, p2p } from './socket.js';
 
 // Top level React component for the text-chat window
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      messages: []
-    }
+      messages: [],
+    };
     p2p.on('message', (newMessage) => {
       this.handleNewMessage(newMessage);
-    })
+    });
   }
   handleMessageSubmit(message) {
-    var {messages} = this.state;
+    const { messages } = this.state;
     messages.unshift(message);
     this.setState({ messages });
-    console.log("handleMessageSubmit", message);
+    console.log('handleMessageSubmit', message);
     p2p.emit('message', message);
   }
 
   handleNewMessage(newMessage) {
-    var {messages} = this.state;
+    const { messages } = this.state;
     messages.unshift(newMessage);
     this.setState({ messages });
   }
@@ -33,10 +33,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <MessageForm submit={this.handleMessageSubmit.bind(this) }/>
+        <MessageForm submit={this.handleMessageSubmit.bind(this)} />
         <MessageContainer messages={this.state.messages} />
       </div>
-    )
+    );
   }
 }
 
