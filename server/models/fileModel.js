994cf3be1997cdbdcc4272e61636026a583f6bc1
Dana Tran <dana.l.tran@gmail.com>
2016-09-13 21:59:05 -0700

    in process - saving file to database

--- server/models/fileModel.js
+++ server/models/fileModel.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 
 
 const fileSchema = new Schema({
-  author: { type: String, required: true, unique: true }, 
-  pathName: { type: String, required: true, unique: true } //, 
+  fileName: { type: String, required: true, unique: true } 
 });
 
 const File = mongoose.model('File', fileSchema);
